331f9e1373f9889d3144223fab43eec253f0e998
mj <farfs>
2026-05-24 20:00:57 +0900

    Add docs and run.sh

---
+++ mydocs/modetest.md
@@ -0,0 +1,6 @@
+핵심 파일은 여기입니다.
+- modetest 본체: output/build/libdrm-2.4.131/tests/modetest/modetest.c
+- 공용 DRM 열기/헬퍼: output/build/libdrm-2.4.131/tests/util/kms.c
+- Buildroot의 libdrm 패키지 설정: package/libdrm/libdrm.mk
+- libdrm 패키지 옵션: p
+
---
+++ run.sh
@@ -0,0 +1,12 @@
+qemu-system-x86_64 \
+  -M pc \
+  -kernel output/images/bzImage \
+  -drive file=output/images/rootfs.ext2,format=raw \
+  -append "root=/dev/sda console=ttyS0 init=/bin/sh" \
+  -vga none \
+  -netdev user,id=eth0 \
+  -device virtio-net-pci,netdev=eth0 \
+  -device virtio-gpu-pci \
+  -display sdl,gl=on \
+  -serial stdio \
+  -virtfs local,path=/home/mj/work/share_folder,mount_tag=host_share,security_model=none,id=host_share
